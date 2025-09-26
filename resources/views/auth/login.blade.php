ff08b536624d0c7726a9c9a2e3db17447ba55941
Salman <149822719+mslmnaa@users.noreply.github.com>
2025-09-26 15:04:04 +0700

    a

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Aino Medical Systemm</title>
+    <title>Login - Aino Medical System</title>
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gradient-to-br from-primary-50 to-secondary-50 min-h-screen flex items-center justify-center">
